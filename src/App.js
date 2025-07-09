06594bf8b5e7155e29ecdbc44772c4423aaf7986
artemdost <dartem925@gmail.com>
2025-07-09 23:22:45 +0300

    last

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import runman3 from "./runman3.png";
 import strongman2 from "./strongman2.png";
 import manshort from "./manshort.png";
 import yogawoman from "./yogawoman.png";
+import noting from "./noting.png";
+import calling from "./calling.png";
 
 function App() {
   return (
@@ -28,7 +30,7 @@ function App() {
                   href="#Programms"
                   className="font-montserrat text-3xl font-medium text-basewhite hover:text-baseorange transition-colors duration-300"
                 >
-                  Программы
+                  Услуги
                 </a>
                 <a
                   href="https://t.me/fititpro"
@@ -48,7 +50,7 @@ function App() {
             <div className="mt-24 flex flex-col w-[600px]">
               <span className="text-7xl font-montserrat font-bold text-basewhite">
                 ТВОЕ ТЕЛО — <br />
-                <span className="text-baseorange">ТВОЯ СИЛА</span>
+                <span className="text-baseorange">ТВОЯ ЭНЕРГИЯ</span>
               </span>
               <span className="mt-5 font-montserrat text-3xl font-thin text-white">
                 Начни путь к лучшей версии себя. <br />
@@ -68,13 +70,61 @@ function App() {
       <div id="Programms" className="font-montserrat bg-basegrey0 pt-5">
         <div className=" bg-basegrey0 flex flex-col  container mx-auto px-6 md:px-12 lg:px-24">
           <span className="mx-auto text-white text-6xl font-semibold">
-            Программы
+            Услуги
           </span>
+          <div className="1BLOCK mt-24 flex relative mx-24 px-12 bg-basegrey1 py-10 mb-10 rounded-[150px]">
+            <div className="mr-2 h-[350px] w-[450px]">
+              <img src={noting} className="h-full object-cover mx-auto"></img>
+            </div>
+
+            <div className="1TEXTBLOCK flex relative flex-col gap-6">
+              <span className="abosulte left-0 mx-9 mt-8 text-5xl text-white font-semibold">
+                <span className="text-baseorange ">Быстрая</span> консультация
+              </span>
+              <div className="text-[27px] m-3 mx-9 text-white">
+                Ответы на вопросы, помощь с выбором программы в персональном
+                чате <br />
+                (30 мин)
+              </div>
+              <button className="mb-9 mx-9 font-montserrat text-3xl font-semibold bg-baseorange text-basegrey0 py-4  rounded-xl w-[230px]  hover:bg-white transition-colors duration-300">
+                Купить
+              </button>
+              <span className="absolute mt-80 right-0 text-[120px] text-baseorange font-semibold">
+                500₽
+              </span>
+            </div>
+            <></>
+          </div>
+          <div className="1BLOCK mt-24 flex relative mx-24 px-12 bg-basegrey1 py-10 mb-10 rounded-[150px]">
+            <div className="mr-2 h-[350px] w-[450px]">
+              <img
+                src={calling}
+                className="h-full object-cover mx-auto mt-1"
+              ></img>
+            </div>
+            <div className="1TEXTBLOCK flex relative flex-col gap-6">
+              <span className="abosulte left-0 mx-9 mt-8 text-5xl text-white font-semibold">
+                <span className="text-baseorange ">Видео</span> консультация
+              </span>
+              <div className="text-[27px] m-3 mx-9 text-white">
+                Ответы на вопросы, помощь с выбором программы по аудио/видео
+                звонку <br />
+                (1 час)
+              </div>
+              <button className="mb-9 mx-9 font-montserrat text-3xl font-semibold bg-baseorange text-basegrey0 py-4  rounded-xl w-[230px]  hover:bg-white transition-colors duration-300">
+                Купить
+              </button>
+              <span className="absolute mt-80 right-0 text-[120px] text-baseorange font-semibold">
+                1000₽
+              </span>
+            </div>
+            <></>
+          </div>
           <div className="1BLOCK mt-24 flex relative mx-24 px-12 bg-basegrey1 py-10 mb-10 rounded-[150px]">
             <img src={runman3} className="w-[300px]"></img>
             <div className="1TEXTBLOCK flex relative flex-col gap-6">
               <span className="abosulte left-0 mx-9 mt-8 text-5xl text-white font-semibold">
-                <span className="text-baseorange">Кардио</span> тренировка
+                <span className="text-baseorange">Кардио</span> программа
               </span>
               <div className="text-[27px] m-3 mx-9 text-white">
                 Энергичная кардио тренировка для сжигания калорий, повышения
@@ -85,26 +135,7 @@ function App() {
                 Купить
               </button>
               <span className="absolute mt-96 right-0 text-[120px] text-baseorange font-semibold">
-                1990₽
-              </span>
-            </div>
-            <></>
-          </div>
-          <div className="1BLOCK mt-24 flex relative mx-24 px-12 bg-basegrey1 py-10 mb-10 rounded-[150px]">
-            <img src={strongman2} className="w-[300px] scale-110"></img>
-            <div className="1TEXTBLOCK flex relative flex-col gap-6">
-              <span className="abosulte left-0 mx-9 mt-8 text-5xl text-white font-semibold">
-                <span className="text-baseorange">Силовая</span> тренировка
-              </span>
-              <div className="text-[27px] m-3 mx-9 text-white">
-                Комплекс упражнений для укрепления мышц, формирования рельефного
-                тела и повышения физической силы. Достигаем целей шаг за шагом!
-              </div>
-              <button className="mx-9 font-montserrat text-3xl font-semibold bg-baseorange text-basegrey0 py-4  rounded-xl w-[230px]  hover:bg-white transition-colors duration-300">
-                Купить
-              </button>
-              <span className="absolute mt-96 right-0 text-[120px] text-baseorange font-semibold">
-                1990₽
+                1500₽
               </span>
             </div>
             <></>
@@ -124,7 +155,27 @@ function App() {
                 Купить
               </button>
               <span className="absolute mt-80 right-0 text-[120px] text-baseorange font-semibold">
-                1990₽
+                2000₽
+              </span>
+            </div>
+            <></>
+          </div>
+          <div className="1BLOCK mt-24 flex relative mx-24 px-12 bg-basegrey1 py-10 mb-10 rounded-[150px]">
+            <img src={strongman2} className="w-[300px] scale-110"></img>
+            <div className="1TEXTBLOCK flex relative flex-col gap-6">
+              <span className="abosulte left-0 mx-9 mt-8 text-5xl text-white font-semibold">
+                <span className="text-baseorange">Силовая</span> программа
+              </span>
+              <div className="text-[27px] m-3 mx-9 text-white">
+                Комплекс упражнений для укрепления мышц, формирования рельефного
+                тела и повышения физической силы. <br /> Уверенный старт для
+                твоего тела!
+              </div>
+              <button className="mx-9 font-montserrat text-3xl font-semibold bg-baseorange text-basegrey0 py-4  rounded-xl w-[230px]  hover:bg-white transition-colors duration-300">
+                Купить
+              </button>
+              <span className="absolute mt-96 right-0 text-[120px] text-baseorange font-semibold">
+                2500₽
               </span>
             </div>
             <></>
